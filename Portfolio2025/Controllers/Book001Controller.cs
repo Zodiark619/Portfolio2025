bed08a5b6fcbdfce21d1988f13bf3b6daa3da70f
asd asd <seonghyejin33@gmail.com>
2025-07-31 15:37:32 +0700

    Add project files.

---
+++ Portfolio2025/Controllers/Book001Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio2025.Models;
+
+namespace Portfolio2025.Controllers
+{
+    public class Book001Controller : Controller
+    {
+        public IActionResult Detail(int id)
+        {
+            Book book = DB.GetBook(id);
+            return View(book);
+        }
+
+        public IActionResult List()
+        {
+            List<Book> books = DB.LoadBooks();
+            return View(books);
+        }
+    }
+}
---
+++ Portfolio2025/Models/Book.cs
@@ -0,0 +1,63 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration.Attributes;
+using System.Globalization;
+
+namespace Portfolio2025.Models
+{
+    public class Book
+    {
+
+        [Name("Id")]
+        public int Id { get; set; }
+        [Name("Book")]
+        public string Title {  get; set; }
+        [Name("Author(s)")]
+        public string Author {  get; set; }
+        [Name("Original language")]
+        public string Language {  get; set; }
+        [Name("First published")]
+        public int FirstPublished {  get; set; }
+        [Name("Approximate sales in millions")]
+        public double SalesInMillions {  get; set; }
+        [Name("Genre")]
+        public string Genre { get; set; }
+    
+
+    }
+
+
+    public static class DB
+    {
+        public static List<Book> LoadBooks()
+        {
+            using (var reader = new StreamReader("best-selling-books.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                return csv.GetRecords<Book>().ToList();
+            }
+        }
+        public static Book GetBook(int id)
+        {
+            List<Book> books = DB.LoadBooks();
+
+            return books.FirstOrDefault(x => x.Id==id);
+        }
+
+
+
+        //foreach (Book p in books)
+        //{
+        //    if (p.Id == id)
+        //    {
+        //        return p;
+        //    }
+        //}
+        //return new Book(); 
+        // return empty Product if not in list
+
+
+    }
+    
+
+
+    }
